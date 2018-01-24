184a002ec2355b918592d47910b185868308daa5
admin@kolabrate.com.au <Ana1982(>
2018-01-24 13:58:29 +1100

    Included Service Bus info.

--- Audit.Core/Channels/Storage/ServiceBus.cs
+++ Audit.Core/Channels/Storage/ServiceBus.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 
 namespace Audit.Core.Channels.Storage
 {
     public class ServiceBus : IChannel
     {
+        private string _serviceBusConnectionString = ConfigurationManager.AppSettings["ServiceBus"];
+        private TopicClient Topic { get; set; }
+
+        //check if queue exists or create a queue
+        public ServiceBus(string serviceBusConnectionString)
+        {
+
+
+            
+        }
+
         public Task<bool> WriteAsync(Models.Audit data)
         {
             throw new NotImplementedException();
-
         }
+
+        
 
     }
 }
